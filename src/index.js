2eb547688e2aa953b94728349ea29f45fd976c99
cielolili <cielito.torres@hotmail.com>
2020-01-27 23:06:40 -0500

    función convierte ruta relativa a absoluta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const path = require('path');
 
 const confirmAbsoluteRoute = (route) => path.isAbsolute(route);
 console.log(path.isAbsolute('/home/cielo/Documentos/Markdown-Links/LIM011-fe-md-links'));
+
+const convertAbsolute = (route) => path.resolve(route);
+console.log(`resolve: ${path.resolve('/foo/bar', './baz')}`);
+console.log(`resolve: ${path.resolve('index.js')}`);
+
+
 const route = {
   confirmAbsoluteRoute,
+  convertAbsolute,
 };
 module.exports = route;
--- test/index.spec.js
+++ test/index.spec.js
@@ -12,3 +12,13 @@ describe('confirmAbsoluteRoute, debe devolver un valor booleano', () => {
     expect(route.confirmAbsoluteRoute('./Documentos/Markdown-Links/LIM011-fe-md-links')).toBe(false);
   });
 });
+describe('convertAbsolute, debe convertir una ruta a absoluta', () => {
+  it('Debería ser una función', () => {
+    expect(typeof route.convertAbsolute).toBe('function');
+  });
+  it('Debería convertir una ruta relativa, a una ruta absoluta', () => {
+    const inputRelative = '../LIM011-fe-md-links';
+    const outPutAbsolute = '/home/cielo/Documentos/Markdown-Links/LIM011-fe-md-links';
+    expect(route.convertAbsolute(inputRelative)).toEqual(route.convertAbsolute(outPutAbsolute));
+  });
+});
